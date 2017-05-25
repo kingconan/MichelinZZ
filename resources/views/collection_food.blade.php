4a1087b7c8d8b5770bba7c9bca30d9bd5559f447
king_from_qq <81465264@qq.com>
2017-05-25 19:04:42 +0800

    add files

---
+++ resources/views/collection_food.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>tmp dev</title>
+    <meta name="keywords" content="关键字" />
+    <meta name="description" content="业务描述" />
+<link rel="stylesheet" href="{{asset('/css/app.css')}}"> 
+   <style>
+        html,body{
+            padding: 0;
+            margin: 0;;
+            width: 100%;
+        }
+        .my_header{
+            padding: 15px;
+        }
+
+        .my_author{
+            font-size: 10px;
+            color:grey;
+            padding-top: 6px;
+        }
+        .my_title{
+            font-size: 14px;
+            font-weight: bolder;
+        }
+        .my_link{
+            display: none;
+        }
+        .my_card{
+            -webkit-box-shadow: 0px 0px 11px -1px rgba(0,0,0,0.38);
+            -moz-box-shadow: 0px 0px 11px -1px rgba(0,0,0,0.38);
+            box-shadow: 0px 0px 11px -1px rgba(0,0,0,0.38);
+            margin: 2px 12px;
+            padding: 12px 8px;
+            cursor: pointer; cursor: hand;
+        }
+        .my_body{
+            max-width: 600px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+</head>
+<body>
+<div style="min-height: 800px">
+    <div class="my_header" style="text-align: center">
+        <h1>REVIEW 4 REVIEWS</h1>
+        <p>xxx</p>
+    </div>
+    <div class="my_body">
+        <div class="my_card" onclick="go(this)">
+            <div style="width: 100%;padding-left: 100px;float: left">
+                <div class="my_link">
+                    <a href="https://mp.weixin.qq.com/s?__biz=MjM5NzI5ODE3Nw==&mid=2655256992&idx=1&sn=fdb1180dd7f7333235b3eb300053fe41&chksm=bd6c88ce8a1b01d86f9888df215b7a9e306d9f780d6208ecee64421eaaf594447eaa98145c40&scene=0&key=9dde87b64dd192c16474be4c27c2a76aeb597761f7475059c72f8f4fa4894d549818f5cc769622baa4940f243befcfb4a355fa4fd90be2142990288dae0a5e097c26f313b247253a66af36a212cf1a80&ascene=0&uin=MTI5ODE0NzE0MA%3D%3D&devicetype=iMac+MacBookPro11%2C3+OSX+OSX+10.11.4+build(15E65)&version=12020110&nettype=WIFI&fontScale=100&pass_ticket=ovjOJE0EQC073PNM9j84dAbxM7FcNIbkfsRa5lkmo%2Fy46aoPsbGp1Lhs8JVL8Edp">原文地址</a>
+                </div>
+                <div class="my_title"><span style="color:white;background-color: black;font-size: 10px;padding: 3px;margin-right: 8px">转载</span>吃完 62 只粽子，这份「买粽指南」让你有个美好的端午</div>
+                <div class="my_author">吃不胖的神秘天团 <span style="color: darkolivegreen;">2017-05-23</span> 发表在 <span style="color: darkolivegreen;">@什么值得吃</span></div>
+                <div style="height: 1px;background-color: gainsboro;margin: 10px 0px"></div>
+                <div class="my_review" >
+                    <span style="color: red">宅记：</span>南咸北甜，咸棕党必然要维护咸蛋黄肉粽的地位！！趁热剥开香味阵阵，肥瘦相间不显得纯肉的干硬，嘴上的一抹油才显示真切。当然除了作者推荐的各品牌中外，小宅独家推荐魔都弄堂口的小阿姨小脚棕~~~
+                </div>
+            </div>
+            <img src="http://pic.pimg.tw/bimay/1382961772-1447132522.jpg?v=1382961773" style="width:100px;height:100px;float: left;margin-left: -100%"/>
+            <div style="clear: both"></div>
+        </div>
+        <div style="height: 10px"></div>
+        <div class="my_card" onclick="go(this)">
+            <div style="width: 100%;padding-left: 100px;float: left">
+                <div class="my_link">
+                    <a href="https://mp.weixin.qq.com/s?__biz=MzI2MTU5NjYyOA==&mid=2247484743&idx=1&sn=a48d2ab1ad92f24486e9aad1d1f20cdc&chksm=ea56b1fcdd2138eaefc86221d55a1cb6926f426aeda6fbb59df68562bb2a55f57b50be908b30&mpshare=1&scene=1&srcid=0509W5hbw5NCgy6WbQ37FU0b&key=9dde87b64dd192c10412d2f84c64f8187ff54c7f42ab01fec68a37d6da0b2bdb65055cd78bdeaebbd6822855c838d0275c5f2bd9ef5daf5ea0c0b2bc9ecdfeda578f26144833cf88653b16f9ec696f26&ascene=0&uin=MTI5ODE0NzE0MA%3D%3D&devicetype=iMac+MacBookPro11%2C3+OSX+OSX+10.11.4+build(15E65)&version=12020110&nettype=WIFI&fontScale=100&pass_ticket=rCvyvyCPGSxgf3gFDzUijEv5YvMeqeQJp2MPNntSA6TvJVG4AQ742F2%2BOTJOFrAp">原文地址</a>
+                </div>
+                <div class="my_title"><span style="color:white;background-color: black;font-size: 10px;padding: 3px;margin-right: 8px">转载</span>《老上海人的鸡蛋软饼测评》地下情报局再次出动！再不吃就吃不到了！</div>
+                <div class="my_author">葛大军 <span style="color: darkolivegreen;">2017-05-09</span> 发表在 <span style="color: darkolivegreen;">疙瘩美食</span></div>
+                <div style="height: 1px;background-color: gainsboro;margin: 10px 0px"></div>
+                <div class="my_review" >
+
+                </div>
+            </div>
+            <img src="http://pic.pimg.tw/bimay/1382961772-1447132522.jpg?v=1382961773" style="width:100px;height:100px;float: left;margin-left: -100%"/>
+            <div style="clear: both"></div>
+        </div>
+    </div>
+</div>
+<div style="background-color: black;min-height: 400px">
+    <div style="max-width: 600px;text-align: center;margin-left: auto;margin-right: auto;padding: 30px">
+        <p style="color: grey">
+            友情链接
+        </p>
+    </div>
+</div>
+<script src="{{ asset('/js/app.js')}}"></script>
+<script>
+    function go(self){
+        var link = $(self).find("a").attr("href");
+        window.location = link;
+    }
+</script>
+</body>
+</html>
